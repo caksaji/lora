20af5c9eeade397549d3524fb61c1a3c4885133e
Aji <caksaaji@gmail.com>
2025-06-16 19:49:13 +0700

    [ADD] new component : Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,33 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./global.sass";
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import '@/app/style/global.sass'
+import Navbar from '@/component/partial/Navbar'
 
 const fontSans = Poppins({
-  subsets: ["latin"],
-  weight: "400"
-});
+  subsets: ['latin'],
+  weight: ['400', '600']
+})
+const menu = [
+  { name: 'Report', link: '/' },
+  { name: 'Product', link: '/product' }
+]
 
 export const metadata: Metadata = {
-  title: "Lora",
-  description: "Product purchase analyzer, built with next.js by https://linkedin.com/in/caksaji",
-};
+  title: 'Lora',
+  description: 'Product purchase analyzer, built with next.js by https://linkedin.com/in/caksaji',
+}
 
 export default function RootLayout({
   children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${fontSans.variable} antialiased`}
-      >
+      <body className="min-h-dvh">
+        <Navbar />
         {children}
       </body>
     </html>
-  );
+  )
 }
