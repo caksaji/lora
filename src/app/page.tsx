c071daa431371bfde48f8c48855e58db4a3933b6
Aji <caksaaji@gmail.com>
2025-06-18 17:38:58 +0700

    [INSTALL] new package : apexcharts@^4.7.0, react-apexcharts@^1.7.0 | uninstall: recharts@^2.15.3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { dateShownFormat, timeShownFormat, formatCurrency } from '@/lib/localUtil'
+import dynamic from 'next/dynamic'
+import { useBreakpoint } from '@/hook/useBreakpoint'
+import { dateShownFormat, timeShownFormat, formatNum, formatCurrency } from '@/lib/localUtil'
 import Skeleton from '@/component/partial/Skeleton'
 import Table from '@/component/partial/Table'
 import IconSvg from '@/component/partial/IconSvg'
-
 import {
   ResponsiveContainer,
   BarChart,
@@ -17,27 +18,35 @@ import {
   Line
 } from 'recharts'
 
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
+
 export default function Report() {
+  const bp = useBreakpoint()
   const highlightData = {
     revenue: 11111,
     sale: 22222,
-    hiSale: 33333,
-    loSale: 44444,
+    hiSale: 'Product Aaaaa',
+    loSale: 'Product Zzzzz',
     productChart: [
       { name: 'Product A', sales: 4000 },
       { name: 'Product B', sales: 3000 },
       { name: 'Product C', sales: 2000 },
       { name: 'Product D', sales: 2780 },
-      { name: 'Product E', sales: 1890 }
+      { name: 'Product E', sales: 1890 },
+      { name: 'Product F', sales: 4000 },
+      { name: 'Product G', sales: 3000 },
+      { name: 'Product H', sales: 2000 },
+      { name: 'Product I', sales: 2780 },
+      { name: 'Product J', sales: 1890 }
     ],
     saleChart: [
-      { name: '2025-03-15', value: 4000 },
-      { name: '2025-03-16', value: 3000 },
-      { name: '2025-03-17', value: 2000 },
-      { name: '2025-03-18', value: 2780 },
-      { name: '2025-03-19', value: 1890 },
-      { name: '2025-03-20', value: 2780 },
-      { name: '2025-03-21', value: 1890 }
+      { date: '2025-03-15', value: 4000 },
+      { date: '2025-03-16', value: 2000 },
+      { date: '2025-03-17', value: 3000 },
+      { date: '2025-03-18', value: 2780 },
+      { date: '2025-03-19', value: 1890 },
+      { date: '2025-03-20', value: 2780 },
+      { date: '2025-03-21', value: 1890 }
     ],
     history: [
       { date: '2025-03-15T03:24:00', value: 99807000 },
@@ -48,6 +57,66 @@ export default function Report() {
     ]
   }
   const showSkeletonTable = false
+  const chartData = {
+    product: {
+      label: highlightData?.productChart?.slice(0, bp.smallerThan('sm') ? 3 : bp.smallerThan('md') ? 5 : bp.smallerThan('lg') ? 4 : 6).map(v => v.name),
+      data: [{
+        name: 'Sold',
+        data: highlightData?.productChart?.slice(0, bp.smallerThan('sm') ? 3 : bp.smallerThan('md') ? 5 : bp.smallerThan('lg') ? 4 : 6).map(v => v.sales)
+      }]
+    },
+    sale: {
+      label: highlightData?.saleChart?.map(v => dateShownFormat(v.date, 'medium')),
+      data: [{
+        name: 'Value',
+        data: highlightData?.saleChart?.map(v => v.value)
+      }]
+    }
+  }
+  const chartOpt = {
+    product: {
+      chart: {
+        fontFamily: 'Poppins, sans-serif',
+        redrawOnParentResize: true,
+        toolbar: { show: false }
+      },
+      yaxis: {
+        labels: {
+          formatter: function(val) {
+            return formatNum(val)
+          }
+        }
+      },
+      xaxis: { categories: chartData.product.label },
+      fill: { colors: ['#8e51ff'] },
+      dataLabels: {
+        formatter: function(val) {
+          return formatNum(val)
+        }
+      }
+    },
+    sale: {
+      chart: {
+        fontFamily: 'Poppins, sans-serif',
+        redrawOnParentResize: true,
+        toolbar: { show: false },
+        zoom: { enabled: false }
+      },
+      yaxis: {
+        labels: {
+          formatter: function(val) {
+            return formatCurrency(val)
+          }
+        }
+      },
+      xaxis: { categories: chartData.sale.label },
+      fill: { colors: ['#8e51ff'] },
+      stroke: {
+        curve: 'smooth',
+        lineCap: 'round'
+      }
+    }
+  }
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-5">
       <div className="col-span-full md:col-span-2">
@@ -62,7 +131,7 @@ export default function Report() {
                   {i === 0 ? 'Total revenue' : i === 1 ? 'Total sales' : i === 2 ? 'Highest sales' : 'Lowest sales'}
                 </div>
                 <div className="text-lg font-semibold">
-                  {i === 0 ? highlightData?.revenue : i === 1 ? highlightData?.sale : i === 2 ? highlightData?.hiSale : highlightData?.loSale}
+                  {i === 0 ? formatCurrency(highlightData?.revenue) : i === 1 ? formatNum(highlightData?.sale) : i === 2 ? highlightData?.hiSale : highlightData?.loSale}
                 </div>
               </div>
             </div>
@@ -74,15 +143,7 @@ export default function Report() {
           <div className="text-lg font-semibold">
             Top Selling Products
           </div>
-          <ResponsiveContainer height={300}>
-            <BarChart data={highlightData?.productChart}>
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="sales" fill="#8e51ff" />
-            </BarChart>
-          </ResponsiveContainer>
+          <Chart type="bar" series={chartData.product.data} height="300" options={chartOpt.product} />
         </div>
       </div>
       <div className="col-span-full">
@@ -90,7 +151,8 @@ export default function Report() {
           <div className="text-lg font-semibold">
             Sales Value History
           </div>
-          <ResponsiveContainer height={300}>
+          <Chart type="line" series={chartData.sale.data} height="300" options={chartOpt.sale} />
+          {/*<ResponsiveContainer height={300}>
             <LineChart data={highlightData?.saleChart}>
               <XAxis dataKey="name" />
               <YAxis />
@@ -98,7 +160,7 @@ export default function Report() {
               <Legend />
               <Line type="monotone" dataKey="value" stroke="#8e51ff" />
             </LineChart>
-          </ResponsiveContainer>
+          </ResponsiveContainer>*/}
         </div>
       </div>
       <div className="col-span-full">
--- src/lib/localUtil.ts
+++ src/lib/localUtil.ts
@@ -1,12 +1,16 @@
 export function dateShownFormat(date: string, dtStyle: string = 'long') {
   const nd = new Date(date)
-  return new Intl.DateTimeFormat('en', { dateStyle: dtStyle }).format(nd)
+  return new Intl.DateTimeFormat('en-US', { dateStyle: dtStyle }).format(nd)
 }
 export function timeShownFormat(time: string) {
   const nd = new Date(time)
-  return new Intl.DateTimeFormat('en', { timeStyle: 'short', hourCycle: 'h24' }).format(nd)
+  return new Intl.DateTimeFormat('en-US', { timeStyle: 'short', hourCycle: 'h24' }).format(nd)
 }
-export function formatCurrency(number: number) {
+export function formatNum(number: number | string) {
   const num = Number(number)
-  return new Intl.NumberFormat('en', { style: 'currency', currency: 'IDR', currencyDisplay: 'code', minimumFractionDigits: 0 }).format(num)
+  return new Intl.NumberFormat('en-US').format(num)
+}
+export function formatCurrency(number: number | string) {
+  const num = Number(number)
+  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'IDR', currencyDisplay: 'code', minimumFractionDigits: 0 }).format(num)
 }
